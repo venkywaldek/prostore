37aab40fcfd0ad55f2e4082d8551ce47155ef9a7
Venkat <venkat@Venkats-MacBook-Air.local>
2025-01-05 10:25:54 +0200

    creating product card page and product list component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,12 @@
+import sampleData from '@/db/sample-data';
+import ProductList from '@/components/shared/product/product-list';
 const Homepage = () => {
-  return <>Pro store</>;
+
+  return (
+    <>
+      <ProductList data={sampleData.products} title='Newest Arrivals' limit={4} />
+    </>
+  );
 };
 
 export default Homepage;
