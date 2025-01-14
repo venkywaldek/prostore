7154bff313097fc028b373dea81a9a9613be7c3a
Venkat <venkat@Venkats-MacBook-Air.local>
2025-01-14 11:02:37 +0200

    added brand to the prisma and ran migrations

---
+++ auth.ts
@@ -0,0 +1,54 @@
+import NextAuth from 'next-auth';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { prisma } from '@/db/prisma';
+import { compareSync } from 'bcrypt-ts-edge';
+export const config = {
+  pages: {
+    signIn: '/sign-in',
+    error: '/sign-in',
+  },
+  session: {
+    strategy: 'jwt',
+    maxAge: 30 * 24 * 60 * 60, //30 days
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: { type: 'email' },
+        password: { type: 'password' },
+      },
+      async authorize(credentials) {
+        if (credentials == null) return null;
+
+        //Find user in database
+        const user = await prisma.user.findFirst({
+          where: {
+            email: credentials.email as string,
+          },
+        });
+        //check is user exists and if the password matches
+        if (user && user.password) {
+          const isMatch = compareSync(
+            credentials.password as string,
+            user.password
+          );
+
+          //If password is correct return user
+          if (isMatch) {
+            return {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+              role: user.role,
+            };
+          }
+        }
+        
+      },
+    }),
+  ],
+};
+
+export const { handlers, auth, signIn, SignOut } = NextAuth(config);
