42f1e8980c1325847fef0610fc182564ea97fce5
Venkat <venkat@Venkats-MacBook-Air.local>
2025-01-18 16:56:58 +0200

    Sign Up Error Handling

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,7 +5,7 @@ import { signIn, signOut } from '@/auth';
 import { isRedirectError } from 'next/dist/client/components/redirect-error';
 import { hashSync } from 'bcrypt-ts-edge';
 import { prisma } from '@/db/prisma';
-
+import { formatError } from '../utils';
 //Sign in the user with credentials
 
 export async function signInWithCredentials(
@@ -66,6 +66,6 @@ export async function signUpUser(prevState: unknown, formData: FormData) {
       throw error;
     }
 
-    return { success: false, message: 'User was not registered' };
+    return { success: false, message: formatError(error) };
   }
 }
