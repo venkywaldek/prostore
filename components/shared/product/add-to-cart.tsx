0106bfe00f71198872a5ede17c02cac81698b853
Venkat <venkat@Venkats-MacBook-Air.local>
2025-01-28 19:37:43 +0200

    Remove Cart Action

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -24,7 +24,7 @@ const AddToCart = ({ item }: { item: CartItem }) => {
 
     //Handle success add to cart
     toast({
-      description: `${item.name} added to cart`,
+      description: res.message,
       action: (
         <ToastAction
           className='bg-primary text-white hover:bg-gray-800'
