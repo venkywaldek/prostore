bcac91ad1fa18e83f91baf2362aed214ec6c4ca2
Venkat <venkat@Venkats-MacBook-Air.local>
2025-01-31 20:04:24 +0200

    Sub total Card

--- app/(root)/cart/cart-table.tsx
+++ app/(root)/cart/cart-table.tsx
@@ -16,6 +16,8 @@ import {
   TableCell,
 } from '@/components/ui/table';
 import { Button } from '@/components/ui/button';
+import { formatCurrency } from '@/lib/utils';
+import { Card, CardContent } from '@/components/ui/card';
 
 const CartTable = ({ cart }: { cart?: Cart }) => {
   const router = useRouter();
@@ -89,9 +91,7 @@ const CartTable = ({ cart }: { cart?: Cart }) => {
                         type='button'
                         onClick={() =>
                           startTransition(async () => {
-                            const res = await addItemToCart(
-                              item
-                            );
+                            const res = await addItemToCart(item);
 
                             if (!res.success) {
                               toast({
@@ -109,14 +109,36 @@ const CartTable = ({ cart }: { cart?: Cart }) => {
                         )}{' '}
                       </Button>
                     </TableCell>
-                    <TableCell className='text-right'>
-${item.price}
-                    </TableCell>
+                    <TableCell className='text-right'>${item.price}</TableCell>
                   </TableRow>
                 ))}
               </TableBody>
             </Table>
           </div>
+          <Card>
+            <CardContent className='p-4  gap-4'>
+              <div className='pb-3 text-xl'>
+                Subtotal ({cart.items.reduce((a, c) => a + c.qty, 0)}):
+                <span className='font-bold'>
+                  {formatCurrency(cart.itemsPrice)}{' '}
+                </span>
+              </div>
+              <Button
+                className='w-full'
+                disabled={isPending}
+                onClick={() =>
+                  startTransition(() => router.push('/shipping-address'))
+                }
+              >
+                {isPending ? (
+                  <Loader className='h-4 w-4 animate-spin' />
+                ) : (
+                  <ArrowRight className='w-4 h-4 ' />
+                )}{' '}
+                Proceed to Checkout
+              </Button>
+            </CardContent>
+          </Card>
         </div>
       )}
     </>
