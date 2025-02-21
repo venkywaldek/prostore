b278e1414fbbdb8ac72cef7834dec2fbe983f301
Venkat Battina <venkatneoiit@gmail.com>
2025-02-21 08:15:01 +0200

    changing footer and header and appnames to venkat softwares

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: {
-    template: `%s | Prostore`,
+    template: `%s | Venkat Softwares`,
     default: APP_NAME,
   },
   description: APP_DESCRIPTION,
--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className='border-t'>
       <div className='p-5 flex-center'>
-        {currentYear} {APP_NAME} Venkat Kumar. All Rights Reserved
+      &copy;  {currentYear} {APP_NAME}  All Rights Reserved
       </div>
     </footer>
   );
