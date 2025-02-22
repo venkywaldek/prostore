2e72d6991e3fca792bfbc3f7754eb7a8f96fb46b
Venkat Battina <venkatneoiit@gmail.com>
2025-02-22 18:25:07 +0200

    making rating as string in latestproducts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,14 @@
-
 import ProductList from '@/components/shared/product/product-list';
-import  {getLatestProducts}  from '@/lib/actions/product.actions';
+import { getLatestProducts } from '@/lib/actions/product.actions';
 
 const Homepage = async () => {
   const latestProducts = await getLatestProducts();
   return (
     <>
       <ProductList
-        data={latestProducts.map((product)=> ({
+        data={latestProducts.map((product) => ({
           ...product,
-          rating: Number(product.rating)
+          rating: product.rating.toString(),
         }))}
         title='Newest Arrivals'
         limit={4}
