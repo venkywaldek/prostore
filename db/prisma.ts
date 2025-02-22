167607d1dd409efd6c0b0ec4f266a290c684a502
Venkat Battina <venkatneoiit@gmail.com>
2025-02-22 18:47:10 +0200

    taking off the prisma fix

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,49 +1,39 @@
 import { Pool, neonConfig } from '@neondatabase/serverless';
 import { PrismaNeon } from '@prisma/adapter-neon';
+
 import { PrismaClient } from '@prisma/client';
 import ws from 'ws';
 
-// Ensure Prisma runs in the Node.js environment
+//Ensure Prisma runs in the Node.js environment
 export const runtime = 'nodejs';
 
-// Setup WebSocket connections, which enables Neon to use WebSocket communication.
+//Setup WebSocket connections, which enables Neon to use Websocket communication.
+
 neonConfig.webSocketConstructor = ws;
 
-// Create a connection pool using the provided connection string
 const connectionString = `${process.env.DATABASE_URL}`;
+
+//Creates a new connection pool using the provided connection string, allowing multiple concurrent connections.
 const pool = new Pool({ connectionString });
 
-// Instantiate the Prisma adapter using the Neon connection pool
+//Instantiate the Prisma adapter using the Neon connection pool to handle the connection between Prisma and neon
 const adapter = new PrismaNeon(pool);
 
-// Define a global type to ensure proper type checking
-type ExtendedPrismaClient = PrismaClient & {
-  product: {
-    price: () => string;
-    rating: () => string;
-  };
-};
-
-// Handle global instance management
-const globalForPrisma = global as unknown as { prisma?: ExtendedPrismaClient };
-
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({ adapter }).$extends({
-    result: {
-      product: {
-        price: {
-          compute(product) {
-            return product.price.toString();
-          },
+//Extends the PrismaClient with custom result transformer to convert the price and rating fields to strings.
+export const prisma = new PrismaClient({ adapter }).$extends({
+  result: {
+    product: {
+      price: {
+        compute(product) {
+          return product.price.toString();
         },
-        rating: {
-          compute(product) {
-            return product.rating.toString();
-          },
+      },
+      rating: {
+        compute(product) {
+          return product.rating.toString();
         },
       },
     },
-  }) as unknown as ExtendedPrismaClient;
+  },
+});
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
