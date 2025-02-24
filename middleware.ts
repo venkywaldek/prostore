5001ca046ba8216877b8ca2260a008da4ae6f53c
Venkat Battina <venkatneoiit@gmail.com>
2025-02-24 08:14:47 +0200

    modifying middlewarejs to using getToken({ req }) from next-auth/jwt, you retrieve the session token in a way that is Edge Runtime–compatible

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,19 @@
-
-import { auth } from '@/auth';
 import { NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
 
 export async function middleware(req: Request) {
-  const session = await auth();
-
-  //Redirect unauthenticated users to sign in
-  if (!session) {
+  // Retrieve the token from the request using NextAuth's JWT helper,
+  // which is compatible with the Edge Runtime.
+  const token = await getToken({ req });
+  
+  // Redirect unauthenticated users to sign in
+  if (!token) {
     return NextResponse.redirect(new URL('/sign-in', req.url));
   }
-
+  
   return NextResponse.next();
 }
 
 export const config = {
   matcher: ['/cart', '/shipping-address'],
 };
-
