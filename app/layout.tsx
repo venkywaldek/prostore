ba583e9246e9fbde087e14277923290458450ba9
Venkat Battina <venkatneoiit@gmail.com>
2025-02-28 08:45:56 +0200

    changing the meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: {
-    template: `%s | Venkat Softwares`,
+    template: `%s | Venkat Prostore`,
     default: APP_NAME,
   },
   description: APP_DESCRIPTION,
