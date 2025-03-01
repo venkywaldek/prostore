9a772462c5375a2b46459069ab4b5c8159f7a42b
Venkat Battina <venkatneoiit@gmail.com>
2025-03-01 10:41:41 +0200

    reverting back to middleware modified file

---
+++ app/api/auth/[...nextauth]/auth-and-route.ts
@@ -0,0 +1,125 @@
+// import NextAuth from 'next-auth';
+// import { PrismaAdapter } from '@auth/prisma-adapter';
+// import CredentialsProvider from 'next-auth/providers/credentials';
+// import { prisma } from '@/db/prisma';
+// import { compare } from 'bcrypt-ts-edge';
+// import type { NextAuthConfig } from 'next-auth';
+// import { NextResponse } from 'next/server';
+
+// // Ensure runtime is set to nodejs
+// export const runtime = 'nodejs';
+
+// export const authConfig: NextAuthConfig = {
+//   pages: {
+//     signIn: '/sign-in',
+//     error: '/sign-in',
+//   },
+//   session: {
+//     strategy: 'jwt' as const,
+//     maxAge: 30 * 24 * 60 * 60, // 30 days
+//   },
+//   adapter: PrismaAdapter(prisma), // Use Prisma adapter for authentication
+//   providers: [
+//     CredentialsProvider({
+//       credentials: {
+//         email: { type: 'email' },
+//         password: { type: 'password' },
+//       },
+//       async authorize(credentials) {
+//         if (credentials == null) return null;
+
+//         // Find user in database
+//         const user = await prisma.user.findFirst({
+//           where: {
+//             email: credentials.email as string,
+//           },
+//         });
+
+//         // Check if user exists and if the password matches
+//         if (user && user.password) {
+//           const isMatch = await compare(
+//             credentials.password as string,
+//             user.password
+//           );
+
+//           // If password is correct, return user
+//           if (isMatch) {
+//             return {
+//               id: user.id,
+//               name: user.name,
+//               email: user.email,
+//               role: user.role,
+//             };
+//           }
+//         }
+
+//         // If user does not exist or password does not match
+//         return null;
+//       },
+//     }),
+//   ],
+//   callbacks: {
+//     async session({ session, user, trigger, token }: any) {
+//       // Set the user ID from the token
+//       session.user.id = token.sub;
+//       session.user.role = token.role;
+//       session.user.name = token.name;
+
+//       console.log(token);
+//       // If there is an update, set the user name
+//       if (trigger === 'update') {
+//         session.user.name = user.name;
+//       }
+//       return session;
+//     },
+
+//     async jwt({ token, user, trigger, session }: any) {
+//       // Assign user fields to token
+//       if (user) {
+//         token.id = user.id;
+//         token.role = user.role;
+
+//         // If user has no name, then use the first part of the email
+//         if (user.name === 'NO_NAME') {
+//           token.name = user.email!.split('@')[0];
+
+//           // Update database to reflect the token name
+//           await prisma.user.update({
+//             where: { id: user.id },
+//             data: { name: token.name },
+//           });
+//         }
+//       }
+//       return token;
+//     },
+
+//     authorized({ request, auth }: any) {
+//       const cartCookie = request.cookies.get('sessionCartId');
+//       // Check for session cart cookie
+//       if (!cartCookie) {
+//         // Generate new session cart id cookie
+//         const sessionCartId = crypto.randomUUID();
+
+//         // Clone the req headers
+//         const newRequestHeaders = new Headers(request.headers);
+
+//         // Create a new response and add the new headers
+//         const response = NextResponse.next({
+//           request: {
+//             headers: newRequestHeaders,
+//           },
+//         });
+
+//         // Set newly generated sessionCartId in the response cookies
+//         response.cookies.set('sessionCartId', sessionCartId);
+//         return response;
+//       } else {
+//         return true;
+//       }
+//     },
+//   },
+// } satisfies NextAuthConfig;
+
+// // Now exporting the NextAuth handler as the default export
+// // eslint-disable-next-line import/no-anonymous-default-export
+// export default (req: Request, res: Response) => NextAuth(req, res, authConfig);
--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,22 @@
-// import { NextResponse } from 'next/server';
-// import { getToken } from 'next-auth/jwt';
+import { NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
 
-import { runtime } from './db/prisma';
+// import { runtime } from './db/prisma';
 
-export { auth as middleware } from '@/auth';
+// export { auth as middleware } from '@/auth';
 
-// export async function middleware(req: Request) {
-//   // Retrieve the token from the request using NextAuth's JWT helper,
-//   // which is compatible with the Edge Runtime.
-//   const token = await getToken({ req });
+export async function middleware(req: Request) {
+  // Retrieve the token from the request using NextAuth's JWT helper,
+  // which is compatible with the Edge Runtime.
+  const token = await getToken({ req });
 
-//   // Redirect unauthenticated users to sign in
-//   if (!token) {
-//     return NextResponse.redirect(new URL('/sign-in', req.url));
-//   }
+  // Redirect unauthenticated users to sign in
+  if (!token) {
+    return NextResponse.redirect(new URL('/sign-in', req.url));
+  }
 
-//   return NextResponse.next();
-// }
+  return NextResponse.next();
+}
 
 export const config = {
   matcher: ['/cart', '/shipping-address'],
