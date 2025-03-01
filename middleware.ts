1c8dc14993b13fcb19b65a516591b00841be71db
Venkat Battina <venkatneoiit@gmail.com>
2025-03-01 10:16:56 +0200

    modifying middleware to the original

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,19 @@
-import { NextResponse } from 'next/server';
-import { getToken } from 'next-auth/jwt';
+// import { NextResponse } from 'next/server';
+// import { getToken } from 'next-auth/jwt';
+export {auth as middleware} from '@/auth'
 
-export async function middleware(req: Request) {
-  // Retrieve the token from the request using NextAuth's JWT helper,
-  // which is compatible with the Edge Runtime.
-  const token = await getToken({ req });
+// export async function middleware(req: Request) {
+//   // Retrieve the token from the request using NextAuth's JWT helper,
+//   // which is compatible with the Edge Runtime.
+//   const token = await getToken({ req });
   
-  // Redirect unauthenticated users to sign in
-  if (!token) {
-    return NextResponse.redirect(new URL('/sign-in', req.url));
-  }
+//   // Redirect unauthenticated users to sign in
+//   if (!token) {
+//     return NextResponse.redirect(new URL('/sign-in', req.url));
+//   }
   
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
 export const config = {
   matcher: ['/cart', '/shipping-address'],
