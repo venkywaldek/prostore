e32b1a07293e484bf3fb5d0db281c083fba1f073
Venkat Battina <venkatneoiit@gmail.com>
2025-03-01 10:22:36 +0200

    adding runtime: nodejs in middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,24 @@
 // import { NextResponse } from 'next/server';
 // import { getToken } from 'next-auth/jwt';
-export {auth as middleware} from '@/auth'
+
+import { runtime } from './db/prisma';
+
+export { auth as middleware } from '@/auth';
 
 // export async function middleware(req: Request) {
 //   // Retrieve the token from the request using NextAuth's JWT helper,
 //   // which is compatible with the Edge Runtime.
 //   const token = await getToken({ req });
-  
+
 //   // Redirect unauthenticated users to sign in
 //   if (!token) {
 //     return NextResponse.redirect(new URL('/sign-in', req.url));
 //   }
-  
+
 //   return NextResponse.next();
 // }
 
 export const config = {
   matcher: ['/cart', '/shipping-address'],
+  runtime: 'nodejs', //Forces Node.js runtime instead of edge
 };
